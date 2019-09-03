21a09f3d14af2e73699e4502c610707a43f49886
autobot123 <52976213+autobot123@users.noreply.github.com>
2019-09-03 19:05:45 +0100

    reformat

--- main/scripts/krpc_FUNCTIONS.py
+++ main/scripts/krpc_FUNCTIONS.py
@@ -12,16 +12,12 @@ from krpc_FUNCTIONS import Core, Launcher, Orbit
 
 class Core:
 
-    def __init__(self, sas_activated=True, launch_num_stages=1, script_name="kRPC script"):
+    def __init__(self, script_name="kRPC script"):
         # connections
         self.conn = krpc.connect(name=script_name)
         self.canvas = self.conn.ui.stock_canvas
         self.vessel = self.conn.space_center.active_vessel
         self.srf_frame = self.vessel.orbit.body.reference_frame
-
-        # launch parameters
-        self.launch_num_stages = launch_num_stages
-        self.vessel.control.sas = sas_activated
 
         # flight info
         self.ut = self.conn.add_stream(getattr, self.conn.space_center, 'ut')
@@ -30,18 +26,9 @@ class Core:
         self.periapsis = self.conn.add_stream(getattr, self.vessel.orbit, 'periapsis_altitude')
         self.stage_1_resources = self.vessel.resources_in_decouple_stage(stage=1, cumulative=False)
         self.srb_fuel = self.conn.add_stream(self.stage_1_resources.amount, 'SolidFuel')
-        self.turn_angle = 0
-
-
-    def print_info(self):
-        while True:
-            print(self.altitude())
-            print(self.apoapsis())
-            # print(self.stage_2_resources)
-            print(self.srb_fuel())
-            time.sleep(1)
 
     def sas_prograde(self):
+        ## todo: one method for sas? pass in sas_mode as arg
         self.vessel.auto_pilot.disengage()
         self.vessel.control.sas = True
         time.sleep(0.1)
@@ -85,19 +72,26 @@ class Core:
 
 class Launcher(Core):
 
-    # core methods
-    def launch(self, warp=0):
+    def __init__(self, script_name='kRPC script', target_apo=100000, target_peri=100000):
+        super().__init__(script_name=script_name)
+        self.target_apo = target_apo
+        self.target_peri = target_peri
+        print("Launch parameters: Target apo = {}    Target peri = {}"
+        .format(self.target_apo, self.target_peri))
+
+    def launch(self, launch_num_stages=1, sas_activated=True, warp=0):
         self.set_phys_warp(warp)
         print("Launching vessel")
         self.vessel.control.throttle = 1
-        for i in range(self.launch_num_stages):
+        for i in range(launch_num_stages):
             self.vessel.control.activate_next_stage()
-        self.vessel.control.sas = True
+        self.vessel.control.sas = sas_activated
         time.sleep(0.1)
 
-    def gravity_turn(self, alt_turn_start=3000, alt_turn_end=20000, target_apo=100000, final_pitch=25, warp=0):
+    def gravity_turn(self, alt_turn_start=3000, alt_turn_end=20000, final_pitch=25, warp=0):
 
         srbs_separated = False
+        turn_angle = 0
 
         self.vessel.auto_pilot.engage()
         self.vessel.auto_pilot.target_pitch_and_heading(90, 90)
@@ -112,10 +106,9 @@ class Launcher(Core):
             if self.altitude() > alt_turn_start and self.altitude() < alt_turn_end:
                 frac = ((self.altitude() - alt_turn_start) / (alt_turn_end - alt_turn_start))
                 new_turn_angle = frac * 90
-                if abs(new_turn_angle - self.turn_angle) > 0.5:
+                if abs(new_turn_angle - turn_angle) > 0.5:
                     turn_angle = 90-new_turn_angle + frac*final_pitch
                     self.vessel.auto_pilot.target_pitch_and_heading(turn_angle, 90)
-                    # print("alt: {},    turn:{}".format(self.altitude(), turn_angle))
 
             if not srbs_separated:
                 if self.srb_fuel() < 0.1:
@@ -124,7 +117,7 @@ class Launcher(Core):
                     self.vessel.control.activate_next_stage()
                     srbs_separated = True
 
-            if self.apoapsis() > target_apo:
+            if self.apoapsis() > self.target_apo:
                 break
 
             time.sleep(0.1)
@@ -134,9 +127,11 @@ class Launcher(Core):
         self.vessel.control.throttle = 0
 
 
-    def gravity_turn_no_staging(self, alt_turn_start=3000, alt_turn_end=20000, target_apo=100000, final_pitch=25, warp=0):
+    def gravity_turn_no_staging(self, alt_turn_start=3000, alt_turn_end=20000, final_pitch=25, warp=0):
 
         ##todo improve turn logic. get it smoother. what to do with final pithc and where to point?
+
+        turn_angle = 0
 
         self.vessel.auto_pilot.engage()
         self.vessel.auto_pilot.target_pitch_and_heading(90, 90)
@@ -151,12 +146,11 @@ class Launcher(Core):
             if self.altitude() > alt_turn_start and self.altitude() < alt_turn_end:
                 frac = ((self.altitude() - alt_turn_start) / (alt_turn_end - alt_turn_start))
                 new_turn_angle = frac * 90
-                if abs(new_turn_angle - self.turn_angle) > 0.5:
+                if abs(new_turn_angle - turn_angle) > 0.5:
                     turn_angle = 90-new_turn_angle + frac*final_pitch
                     self.vessel.auto_pilot.target_pitch_and_heading(turn_angle, 90)
-                    # print("alt: {},    turn:{}".format(self.altitude(), turn_angle))
 
-            if self.apoapsis() > target_apo:
+            if self.apoapsis() > self.target_apo:
                 break
 
             time.sleep(0.1)
@@ -166,11 +160,12 @@ class Launcher(Core):
         self.vessel.control.throttle = 0
 
 
-    def circularise(self, target_peri=80000):
+    def circularise(self):
 
         while self.altitude() < 70000:
             pass
 
+        ## todo create core method
         mu = self.vessel.orbit.body.gravitational_parameter
         r = self.vessel.orbit.apoapsis
         a1 = self.vessel.orbit.semi_major_axis
@@ -180,6 +175,7 @@ class Launcher(Core):
         delta_v = v2 - v1
         node = self.vessel.control.add_node(self.ut() + self.vessel.orbit.time_to_apoapsis, prograde=delta_v)
 
+        ## todo create core method
         F = self.vessel.available_thrust
         Isp = self.vessel.specific_impulse * 9.82
         m0 = self.vessel.mass
@@ -213,7 +209,7 @@ class Launcher(Core):
         self.vessel.control.throttle = 0.05
         remaining_burn =self.conn.add_stream(node.remaining_burn_vector, node.reference_frame)
         # while remaining_burn()[1] > 0:
-        while self.periapsis() < 0.99*target_peri:
+        while self.periapsis() < 0.99*self.target_peri:
             pass
         self.vessel.control.throttle = 0.0
         node.remove()
@@ -328,10 +324,16 @@ class Orbit(Core):
         self.vessel.control.throttle = 1.0
         time.sleep(burn_time - 0.1)
         print('Fine tuning')
-        self.vessel.control.throttle = 0.02
+        self.vessel.control.throttle = 0.05
+        breakpoint()
         remaining_burn = self.conn.add_stream(node.remaining_burn_vector, node.reference_frame)
-        # while remaining_burn()[1] > 0:
-        while self.periapsis() < 0.9999 * self.apoapsis():
+
+        #fixme use node.remaining_delta_v instead? "dumb" method, would have to use some low value like <0.2
+        while remaining_burn()[1] > 0:
+            print("\nRemaining burn vector item 2: {}".format(remaining_burn()[1]))
+            print("Remaining delta v: {}\n".format(node.remaining_delta_v))
+            time.sleep(1)
+
             pass
         self.vessel.control.throttle = 0.0
         node.remove()
@@ -339,12 +341,12 @@ class Orbit(Core):
 
 def main():
 
-    launch = Launcher()
+    launcher = Launcher()
     orbit = Orbit()
 
-    launch.launch()
-    launch.gravity_turn_no_staging()
-    launch.circularise()
+    launcher.launch()
+    launcher.gravity_turn_no_staging()
+    launcher.circularise()
 
 
 if __name__ == "__main__":
